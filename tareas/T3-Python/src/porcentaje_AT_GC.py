9e0bab767e9cdb6e5495f3b323e9437073de6dbb
diegocarcam <diegocar@lcg.unam.mx>
2023-03-28 19:47:27 -0600

    Agregué los archivos correspondientes a la tarea 3

---
+++ tareas/T3-Python/src/porcentaje_AT_GC.py
@@ -0,0 +1,17 @@
+'''
+El programa calcula el porcentaje de GC y AT en la secuencia de DNA
+de un archivo, al cual se accede por medio de la ruta proporcionada 
+como input.
+'''
+
+nombre = input('Ingrese el nombre del archivo con la secuencia de DNA: ')
+ruta = input('Ingrese la ruta al archivo: ')
+
+dna = open(ruta+nombre).read().rstrip('\n')
+
+porcentaje_GC = (dna.count("G") + dna.count("C"))*100/len(dna) 
+porcentaje_AT = (dna.count("A") + dna.count("T"))*100/len(dna)
+
+print(f'El procentaje de GC es: {porcentaje_GC}%\n'
+      f'El porcentaje de AT es: {porcentaje_AT}%\n'
+      )
